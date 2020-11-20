56d64abd07ff7314a93c61efde90eba0e68efa3e
satvik20 <satvikshukla10122000@gmail.com>
2020-11-20 17:41:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/syms/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.syms;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scroll);
+
+
+    }
+}
